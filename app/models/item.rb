46bf4b2bc02dbae8d2002a9d186636144447b189
taka1ad <87715279+taka1ad@users.noreply.github.com>
2021-11-21 21:19:32 +0900

    商品出品機能・修正3（テストコードまで）

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,6 @@ class Item < ApplicationRecord
   has_one_attached :image
   validates :image, :name, :explanation, :category_id, :condition_id, :delivery_charge_id, :delivery_area_id, :delivery_day_id, :price, presence: true
   validates :category_id, :condition_id, :delivery_charge_id, :delivery_area_id, :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, numericality: { only_integer: true }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   
 end
