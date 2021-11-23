111acfb17bdd7f79d16f7c8d096096df3a7245a8
taka1ad <87715279+taka1ad@users.noreply.github.com>
2021-11-23 20:43:47 +0900

    商品詳細表示機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,10 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to :delivery_charge
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_area
+  belongs_to :delivery_day
   has_one_attached :image
   validates :image, :name, :explanation, :category_id, :condition_id, :delivery_charge_id, :delivery_area_id, :delivery_day_id, :price, presence: true
   validates :category_id, :condition_id, :delivery_charge_id, :delivery_area_id, :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
